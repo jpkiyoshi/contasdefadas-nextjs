e6cb0c02b995999cba7659fd72d83c12774f972b
João Paulo Kiyoshi <kiyoshi33@gmail.com>
2021-11-03 17:44:06 -0300

    remove layouts and lib from tailwind purge command

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './layouts/**/*.{js,ts,jsx,tsx}',
-    './lib/**/*.{js,ts,jsx,tsx}'
-  ],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
@@ -13,9 +8,7 @@ module.exports = {
         'contas-pink-main': '#ff9a8b',
         'contas-pink-dark': '#ff6a88',
         'contas-pink-light': '#ff99ac',
-        'contas-purple-dark': '#56001e',
-
-        
+        'contas-purple-dark': '#56001e'
       },
       fontFamily: {
         'contas-roboto': 'roboto slab'
