350aa03cf2eba749f77c27ce064a153ab0ce6e61
João Paulo Kiyoshi <kiyoshi33@gmail.com>
2021-12-08 16:13:13 -0300

    update product details page

--- pages/acessorios/[categoria]/[product].js
+++ pages/acessorios/[categoria]/[product].js
@@ -1,22 +1,45 @@
 import Image from 'next/image';
 import client from '../../../sanity/client';
 import formatMoney from '../../../utils/formatMoney';
+import { useRouter } from 'next/router';
 
 const Product = ({ data }) => {
   const { categoria, descricao, imagem, nome, preco } = data[0];
+  const router = useRouter();
 
   return (
-    <>
-      <h1 className="text-5xl">{categoria}</h1>
-      <div className="relative flex-col justify-center border items-left border-contas-pink-light rounded-xl">
-        <Image src={imagem} height="500" width="500" className="rounded-t-xl" />
-        <div className="relative flex flex-col p-5 space-y-5">
-          <h3 className="w-full text-2xl font-bold break-words">{nome}</h3>
-          <p>{descricao}</p>
-          <p className="text-lg">{formatMoney(parseInt(preco))}</p>
+    <section className="overflow-hidden text-gray-700 body-font bg-contas-pink-light">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-wrap mx-auto lg:w-4/5">
+          <img
+            alt="ecommerce"
+            className="object-cover object-center w-full rounded shadow-lg lg:w-1/2"
+            src={imagem}
+          />
+          <div className="w-full mt-6 lg:w-1/2 lg:pl-10 lg:py-6 lg:mt-0">
+            <h2 className="mb-1 text-sm tracking-widest text-gray-500 uppercase title-font">
+              {categoria}
+            </h2>
+            <h1 className="mb-1 text-3xl font-medium text-gray-900 title-font">
+              {nome}
+            </h1>
+            <p className="mb-3 leading-relaxed">{descricao}</p>
+            <div className="flex flex-col gap-4">
+              <span className="text-2xl font-medium text-gray-900 title-font">
+                {formatMoney(parseInt(preco))}
+              </span>
+              <a
+                target="_blank"
+                href={`https://api.whatsapp.com/send?phone=5561985601755&text=Olá, gostaria de comprar o acessório ${nome}, de R$${preco}. https://contasdefadas-nextjs.vercel.app${router.asPath}`}
+                className="flex justify-center px-6 py-2 text-white bg-green-700 border-0 rounded focus:outline-none hover:bg-green-600"
+              >
+                Comprar por whatsapp
+              </a>
+            </div>
+          </div>
         </div>
       </div>
-    </>
+    </section>
   );
 };
 
