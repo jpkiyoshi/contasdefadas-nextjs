4a570d85296669fd9b0206f5c95e2ab6c18bdab4
João Paulo Kiyoshi <kiyoshi33@gmail.com>
2021-12-14 19:24:24 -0300

    switch analytics script from Head to Layout component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,12 +2,18 @@ import Header from './Header';
 import Footer from './Footer';
 import ScrollToTopButton from './ScrollToTopButton';
 import Meta from './Meta';
+import Script from 'next/script';
 
 export default function Layout({ title, children }) {
   return (
     <div>
       <Meta />
-
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"kiyoshi33@gmail.com",utcoffset:"-3"}))};sessionStorage.setItem("_swa","1");`
+        }}
+      />
       <Header />
 
       <main>{children}</main>
