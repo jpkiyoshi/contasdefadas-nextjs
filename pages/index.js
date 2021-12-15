ffc5cf81452c89b0450553957c481ed7be74d97a
João Paulo Kiyoshi <kiyoshi33@gmail.com>
2021-12-15 16:16:04 -0300

    update home page images

--- pages/index.js
+++ pages/index.js
@@ -13,46 +13,46 @@ export default function Home() {
         title="Colares & Chokers"
         slug="colares"
         description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Non odio ab laboriosam, in optio dolore repellendus quos labore eius voluptas!"
-        imgUrl="https://res.cloudinary.com/jpkiyoshi/image/upload/v1635978147/IMG_3189_ewwloz.jpg"
+        imgUrl="https://res.cloudinary.com/jpkiyoshi/image/upload/v1639595551/fotos-home-page-contas-de-fadas/colares_bkdq3f.png"
       />
       <HomeCards
         isInversed
         slug="pulseiras"
         title="Pulseiras"
         description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Non odio ab laboriosam, in optio dolore repellendus quos labore eius voluptas!"
-        imgUrl="https://res.cloudinary.com/jpkiyoshi/image/upload/v1635974150/pulseiras-home_x2c6wt.jpg"
+        imgUrl="https://res.cloudinary.com/jpkiyoshi/image/upload/v1639595551/fotos-home-page-contas-de-fadas/pulseiras_kgqeyq.png"
       />
       <HomeCards
         slug="tornozeleiras"
         title="Tornozeleiras"
         description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Non odio ab laboriosam, in optio dolore repellendus quos labore eius voluptas!"
-        imgUrl="https://res.cloudinary.com/jpkiyoshi/image/upload/v1635974150/pulseiras-home_x2c6wt.jpg"
+        imgUrl="https://res.cloudinary.com/jpkiyoshi/image/upload/v1639595552/fotos-home-page-contas-de-fadas/tornozeleiras_xlix9z.png"
       />
       <HomeCards
         isInversed
         title="Brincos"
         slug="brincos"
         description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Non odio ab laboriosam, in optio dolore repellendus quos labore eius voluptas!"
-        imgUrl="https://res.cloudinary.com/jpkiyoshi/image/upload/v1635978147/IMG_3189_ewwloz.jpg"
+        imgUrl="https://res.cloudinary.com/jpkiyoshi/image/upload/v1639595553/fotos-home-page-contas-de-fadas/brincos_bun08q.png"
       />
       <HomeCards
         slug="aneis"
         title="Anéis"
         description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Non odio ab laboriosam, in optio dolore repellendus quos labore eius voluptas!"
-        imgUrl="https://res.cloudinary.com/jpkiyoshi/image/upload/v1635974150/pulseiras-home_x2c6wt.jpg"
+        imgUrl="https://res.cloudinary.com/jpkiyoshi/image/upload/v1639595552/fotos-home-page-contas-de-fadas/aneis_odghgi.png"
       />
       <HomeCards
         isInversed
         title="Phone Straps"
         slug="phonestraps"
         description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Non odio ab laboriosam, in optio dolore repellendus quos labore eius voluptas!"
-        imgUrl="https://res.cloudinary.com/jpkiyoshi/image/upload/v1635978147/IMG_3189_ewwloz.jpg"
+        imgUrl="https://res.cloudinary.com/jpkiyoshi/image/upload/v1639595551/fotos-home-page-contas-de-fadas/phonestraps_x954n7.png"
       />
       <HomeCards
         slug="chaveiros"
         title="Chaveiros"
         description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Non odio ab laboriosam, in optio dolore repellendus quos labore eius voluptas!"
-        imgUrl="https://res.cloudinary.com/jpkiyoshi/image/upload/v1635974150/pulseiras-home_x2c6wt.jpg"
+        imgUrl="https://res.cloudinary.com/jpkiyoshi/image/upload/v1639595552/fotos-home-page-contas-de-fadas/chaveiros_gsbxpa.png"
       />
       <Testimonials />
       <FAQ />
