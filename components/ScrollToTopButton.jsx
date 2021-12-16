f4555d65e1963117c015545fc8ddd97ccf1e0b26
João Paulo Kiyoshi <kiyoshi33@gmail.com>
2021-12-16 01:27:33 -0300

    add z-index to scrollToTop button and change order of products from asc to desc

--- components/ScrollToTopButton.jsx
+++ components/ScrollToTopButton.jsx
@@ -1,35 +1,37 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
 const ScrollToTopButton = () => {
-    const [scrollButtonVisible, setScrollButtonVisible] = useState(false);
+  const [scrollButtonVisible, setScrollButtonVisible] = useState(false);
 
-    const toggleVisible = () => {
-      const scrolled = document.documentElement.scrollTop;
-      scrolled > 1000 
-        ? setScrollButtonVisible(true)
-        : setScrollButtonVisible(false)
-    };
-    
-    const scrollToTop = () =>{
-      window.scrollTo({
-        top: 0, 
-        behavior: 'smooth'
-      });
-    };
-    
-    useEffect(() => {
-      window.addEventListener('scroll', toggleVisible);
-    }, []);
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    scrolled > 1000
+      ? setScrollButtonVisible(true)
+      : setScrollButtonVisible(false);
+  };
 
-    return (
-        <div className={`
-          ${(scrollButtonVisible ? " fixed" : " hidden")} 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisible);
+  }, []);
+
+  return (
+    <div
+      className={`
+          ${scrollButtonVisible ? ' fixed' : ' hidden'} 
           w-12 h-12 bottom-5 right-5 p-2 bg-contas-pink-dark text-white font-extrabold cursor-pointer 
-          flex justify-center items-center`}
-          onClick={() => scrollToTop()}>
-          <span>/\</span>
-        </div>
-    );
-}
+          flex justify-center items-center z-10`}
+      onClick={() => scrollToTop()}
+    >
+      <span>/\</span>
+    </div>
+  );
+};
 
 export default ScrollToTopButton;
--- services/sanity.js
+++ services/sanity.js
@@ -2,22 +2,20 @@ import sanityClient from '@sanity/client';
 import { PaginationConfig } from '../utils/pagination';
 
 const client = sanityClient({
-    projectId: '4nd9464x',
-    dataset: 'production',
-    apiVersion: '2021-11-01',
-    token: '', // or leave blank to be anonymous user
-    useCdn: false // `false` if you want to ensure fresh data
+  projectId: '4nd9464x',
+  dataset: 'production',
+  apiVersion: '2021-11-01',
+  token: '', // or leave blank to be anonymous user
+  useCdn: false // `false` if you want to ensure fresh data
 });
 
-
 const createProductQuery = () => {
-    return;
-}
+  return;
+};
 
-const getCategoryQueryString = (category) => {
-    return (
-        `*[_type == "acessorio" && categoria._ref in 
-        *[slug.current == "${category}"]._id] | order(_createdAt) {
+const getCategoryQueryString = category => {
+  return `*[_type == "acessorio" && categoria._ref in 
+        *[slug.current == "${category}"]._id] | order(_createdAt desc) {
         _id,
         "name": nome,
         "category": categoria->categoria,
@@ -26,47 +24,48 @@ const getCategoryQueryString = (category) => {
         "description": descricao,
         "price": preco,
         "productSlug": slug.current,
-        }`
-    )
-}
+        }`;
+};
 
 const createCategoryQuery = (category, page = null) => {
-
-    let sanityPageFormat = '';
-    if (page) {
-        sanityPageFormat = getSanityPageFormat(page);
-    }
-    const query = getCategoryQueryString(category);
-    return `{
+  let sanityPageFormat = '';
+  if (page) {
+    sanityPageFormat = getSanityPageFormat(page);
+  }
+  const query = getCategoryQueryString(category);
+  return `{
         "results": ${query}${sanityPageFormat},
         "totalNumberOfProductsInCategory": count(${query}),
     }`;
-}
+};
 
-const getSanityPageFormat = (page, pageSize = PaginationConfig.config.pageSize) => {
-    const start = page === 1 ? 0 : (page - 1) * pageSize;
-    const finish = (page * pageSize) - 1;
-    return `[${start}..${finish}]`;
-}
+const getSanityPageFormat = (
+  page,
+  pageSize = PaginationConfig.config.pageSize
+) => {
+  const start = page === 1 ? 0 : (page - 1) * pageSize;
+  const finish = page * pageSize - 1;
+  return `[${start}..${finish}]`;
+};
 
-const getQuery = async (query) => {
-    return await client.fetch(query);
-}
+const getQuery = async query => {
+  return await client.fetch(query);
+};
 
-const getTotalProductsInCategory = async (category) => {
-    const query = getCategoryQueryString(category);
-    return await client.fetch(`count(${query})`);
-}
+const getTotalProductsInCategory = async category => {
+  const query = getCategoryQueryString(category);
+  return await client.fetch(`count(${query})`);
+};
 
 const getContentFromSpecificCategoryPage = async (category, page) => {
-    const query = createCategoryQuery(category, page);
-    return await client.fetch(query);
-}
+  const query = createCategoryQuery(category, page);
+  return await client.fetch(query);
+};
 
 const exports = {
-    createProductQuery,
-    getTotalProductsInCategory,
-    getContentFromSpecificCategoryPage,
+  createProductQuery,
+  getTotalProductsInCategory,
+  getContentFromSpecificCategoryPage
 };
 
 export default exports;
