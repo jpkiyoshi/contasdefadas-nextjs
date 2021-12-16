ef3298d63c5436c82b900c04ee4d0c20c7044a9a
João Paulo Kiyoshi <kiyoshi33@gmail.com>
2021-12-16 00:35:01 -0300

    fix pagination buttons

--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -15,37 +15,35 @@ const Pagination = props => {
 
   return (
     <div className="flex items-center mx-auto mb-9">
-      <button className="flex items-center px-4 py-3 mr-2 font-bold border rounded-lg border-contas-purple-dark text-contas-purple-dark hover:bg-contas-purple-dark hover:text-white">
-        {prevDisabled && (
-          <a>
-            <span>&#8592;</span> Anterior
+      {prevDisabled && (
+        <a className="flex items-center invisible px-4 py-3 mr-2 font-bold border rounded-lg border-contas-purple-dark text-contas-purple-dark hover:bg-contas-purple-dark hover:text-white">
+          <span className="mr-1">&#8592;</span> Anterior
+        </a>
+      )}
+      {!prevDisabled && (
+        <Link href={prevPageUrl}>
+          <a className="flex items-center px-4 py-3 mr-2 font-bold border rounded-lg border-contas-purple-dark text-contas-purple-dark hover:bg-contas-purple-dark hover:text-white">
+            <span className="mr-1">&#8592;</span> Anterior
           </a>
-        )}
-        {!prevDisabled && (
-          <Link href={prevPageUrl}>
-            <a>
-              <span>&#8592;</span> Anterior
-            </a>
-          </Link>
-        )}
-      </button>
+        </Link>
+      )}
+
       <div className="text-lg font-bold">
         {currentPage} de {totalPages}
       </div>
-      <button className="flex items-center px-4 py-3 ml-2 font-bold text-white border border-teal-500 rounded-lg bg-contas-purple-dark hover:bg-opacity-90">
-        {nextDisabled && (
-          <a>
-            Seguinte <span>&#8594;</span>
+
+      {nextDisabled && (
+        <a className="flex items-center invisible px-4 py-3 ml-2 font-bold text-white border rounded-lg border-contas-purple-dark bg-contas-purple-dark hover:bg-opacity-90">
+          Seguinte <span className="ml-1">&#8594;</span>
+        </a>
+      )}
+      {!nextDisabled && (
+        <Link href={nextPageUrl}>
+          <a className="flex items-center px-4 py-3 ml-2 font-bold text-white border rounded-lg border-contas-purple-dark bg-contas-purple-dark hover:bg-opacity-90">
+            Seguinte<span className="ml-1">&#8594;</span>
           </a>
-        )}
-        {!nextDisabled && (
-          <Link href={nextPageUrl}>
-            <a>
-              Seguinte <span>&#8594;</span>
-            </a>
-          </Link>
-        )}
-      </button>
+        </Link>
+      )}
     </div>
   );
 };
