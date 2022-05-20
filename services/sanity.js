c430a4f12f290968ec18ca29ac13363e10f60a73
João Paulo Kiyoshi <kiyoshi33@gmail.com>
2022-05-20 19:45:31 -0300

    modify price formatting and optimize sanity client

--- services/sanity.js
+++ services/sanity.js
@@ -1,20 +1,12 @@
-import sanityClient from '@sanity/client';
 import { PaginationConfig } from '../utils/pagination';
-
-const client = sanityClient({
-  projectId: '4nd9464x',
-  dataset: 'production',
-  apiVersion: '2021-11-01',
-  token: '', // or leave blank to be anonymous user
-  useCdn: false // `false` if you want to ensure fresh data
-});
+import client from '../sanity/client';
 
 const createProductQuery = () => {
-  return;
+	return;
 };
 
 const getCategoryQueryString = category => {
-  return `*[_type == "acessorio" && categoria._ref in 
+	return `*[_type == "acessorio" && categoria._ref in 
         *[slug.current == "${category}"]._id] | order(_createdAt desc) {
         _id,
         "name": nome,
@@ -28,44 +20,41 @@ const getCategoryQueryString = category => {
 };
 
 const createCategoryQuery = (category, page = null) => {
-  let sanityPageFormat = '';
-  if (page) {
-    sanityPageFormat = getSanityPageFormat(page);
-  }
-  const query = getCategoryQueryString(category);
-  return `{
+	let sanityPageFormat = '';
+	if (page) {
+		sanityPageFormat = getSanityPageFormat(page);
+	}
+	const query = getCategoryQueryString(category);
+	return `{
         "results": ${query}${sanityPageFormat},
         "totalNumberOfProductsInCategory": count(${query}),
     }`;
 };
 
-const getSanityPageFormat = (
-  page,
-  pageSize = PaginationConfig.config.pageSize
-) => {
-  const start = page === 1 ? 0 : (page - 1) * pageSize;
-  const finish = page * pageSize - 1;
-  return `[${start}..${finish}]`;
+const getSanityPageFormat = (page, pageSize = PaginationConfig.config.pageSize) => {
+	const start = page === 1 ? 0 : (page - 1) * pageSize;
+	const finish = page * pageSize - 1;
+	return `[${start}..${finish}]`;
 };
 
 const getQuery = async query => {
-  return await client.fetch(query);
+	return await client.fetch(query);
 };
 
 const getTotalProductsInCategory = async category => {
-  const query = getCategoryQueryString(category);
-  return await client.fetch(`count(${query})`);
+	const query = getCategoryQueryString(category);
+	return await client.fetch(`count(${query})`);
 };
 
 const getContentFromSpecificCategoryPage = async (category, page) => {
-  const query = createCategoryQuery(category, page);
-  return await client.fetch(query);
+	const query = createCategoryQuery(category, page);
+	return await client.fetch(query);
 };
 
 const exports = {
-  createProductQuery,
-  getTotalProductsInCategory,
-  getContentFromSpecificCategoryPage
+	createProductQuery,
+	getTotalProductsInCategory,
+	getContentFromSpecificCategoryPage,
 };
 
 export default exports;
